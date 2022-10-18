893d112a6937a71ba2782c23ec465aa4ece206d6
Adrian <adrian.e.rosales@gmail.com>
2022-10-18 16:45:56 -0700

    addd sample service for easier testing in render

--- routers/api.js
+++ routers/api.js
@@ -5,6 +5,7 @@ import BaseRouter from './base-router.js';
 // import DemosService from '../services/demos.js';
 // import CustomersService from '../services/customers.js';
 import AIPictureService from '../services/ai-picture-service.js';
+import SampleService from '../services/sample.js';
 
 class APIRouter extends BaseRouter {
 
@@ -23,6 +24,7 @@ class APIRouter extends BaseRouter {
         // const ds = new DemosService(stash, log);
         // const cs = new CustomersService(stash, log);
         const aps = new AIPictureService(stash, log);
+        const ss = new SampleService(stash, log);
 
         
         //routes examples
@@ -30,7 +32,7 @@ class APIRouter extends BaseRouter {
         // router.get('/api/demos-2', ds.readEnvVars.bind(ds));
         // router.get('/api/demos-3', ds.readRemoteData.bind(ds));
         // router.get('/api/demos-4', ds.readJSONData.bind(ds));
-
+        router.get('/api/sample', ss.sayHello.bind(ss));
         // router.post('/api/customers', cs.createCustomers.bind(cs));
         // router.get('/api/customers', cs.readCustomers.bind(cs));
         // router.put('/api/customers',  cs.updateCustomers.bind(cs));
